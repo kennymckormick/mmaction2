16c0308b9accc9f62492de343e714591d9045f09
Kenny <dhd.efz@gmail.com>
2021-01-17 19:52:52 +0800

    remove some unused

--- mmaction/datasets/pipelines/__init__.py
+++ mmaction/datasets/pipelines/__init__.py
@@ -1,33 +1,22 @@
-from .augmentations import (CenterCrop, ColorJitter, Flip, Fuse,
-                            HeatmapFlipTest, MultiGroupCrop, MultiScaleCrop,
-                            Normalize, PoseCompact, PoseFlip,
-                            PoseMultiScaleCrop, PoseRandomResizedCrop,
-                            PoseResize, RandomCrop, RandomRescale,
-                            RandomResizedCrop, Resize, TenCrop, ThreeCrop)
+from .augmentations import (CenterCrop, Flip, Fuse, HeatmapFlipTest, Normalize,
+                            PoseCompact, PoseFlip, RandomCrop, RandomRescale,
+                            RandomResizedCrop, Resize, ThreeCrop)
 from .compose import Compose
 from .formating import (Collect, FormatShape, ImageToTensor, ToDataContainer,
                         ToTensor, Transpose)
 from .loading import (ConvertCompactHeatmap, DecordDecode, DecordInit,
-                      DenseSampleFrames, FrameSelector,
-                      GenerateLocalizationLabels, GeneratePoseTarget,
-                      Heatmap2Potion, LoadFile, LoadKineticsPose,
-                      LoadLocalizationFeature, LoadProposals, PoseDecode,
-                      PoTionDecode, RawFrameDecode, RawRGBFlowDecode,
-                      SampleFrames, SampleProposalFrames, UniformSampleFrames,
-                      UntrimmedSampleFrames)
+                      FrameSelector, GeneratePoseTarget, Heatmap2Potion,
+                      LoadFile, LoadKineticsPose, PoseDecode, PoTionDecode,
+                      RawFrameDecode, RawRGBFlowDecode, SampleFrames,
+                      UniformSampleFrames)
 
 __all__ = [
-    'SampleFrames', 'DecordDecode', 'DenseSampleFrames', 'OpenCVDecode',
-    'FrameSelector', 'MultiGroupCrop', 'MultiScaleCrop', 'RandomResizedCrop',
+    'SampleFrames', 'DecordDecode', 'FrameSelector', 'RandomResizedCrop',
     'RandomCrop', 'Resize', 'Flip', 'Fuse', 'Normalize', 'ThreeCrop',
-    'CenterCrop', 'TenCrop', 'ImageToTensor', 'Transpose', 'Collect',
-    'FormatShape', 'Compose', 'ToTensor', 'ToDataContainer',
-    'GenerateLocalizationLabels', 'LoadLocalizationFeature', 'LoadProposals',
-    'DecordInit', 'SampleProposalFrames', 'UntrimmedSampleFrames',
-    'RawFrameDecode', 'DecordInit', 'SampleProposalFrames', 'ColorJitter',
-    'RawRGBFlowDecode', 'PoseRandomResizedCrop', 'PoseMultiScaleCrop',
-    'PoseResize', 'PoseFlip', 'GeneratePoseTarget', 'PoseDecode',
-    'LoadKineticsPose', 'PoseCompact', 'RandomRescale', 'Heatmap2Potion',
-    'UniformSampleFrames', 'PoTionDecode', 'ConvertCompactHeatmap',
-    'HeatmapFlipTest', 'LoadFile'
+    'CenterCrop', 'ImageToTensor', 'Transpose', 'Collect', 'FormatShape',
+    'Compose', 'ToTensor', 'ToDataContainer', 'DecordInit', 'RawFrameDecode',
+    'DecordInit', 'RawRGBFlowDecode', 'PoseFlip', 'GeneratePoseTarget',
+    'PoseDecode', 'LoadKineticsPose', 'PoseCompact', 'RandomRescale',
+    'Heatmap2Potion', 'UniformSampleFrames', 'PoTionDecode',
+    'ConvertCompactHeatmap', 'HeatmapFlipTest', 'LoadFile'
 ]
