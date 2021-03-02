5801532e473a1113698499f80e8163e686edbf2c
Kenny <dhd.efz@gmail.com>
2021-03-02 15:57:03 +0800

    reimplement sample_by_class

--- mmaction/datasets/samplers/distributed_sampler.py
+++ mmaction/datasets/samplers/distributed_sampler.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import torch
 from torch.utils.data import DistributedSampler as _DistributedSampler
 
@@ -33,6 +35,7 @@ class DistributedSampler(_DistributedSampler):
         return iter(indices)
 
 
+'''
 class DistributedPowerSampler(_DistributedSampler):
     """DistributedPowerSampler inheriting from
     ``torch.utils.data.DistributedSampler``.
@@ -70,5 +73,52 @@ class DistributedPowerSampler(_DistributedSampler):
 
         indices = indices[self.rank:self.total_size:self.num_replicas]
         assert len(indices) == self.num_samples
+        return iter(indices)
+'''
 
+
+class DistributedPowerSampler(_DistributedSampler):
+    """DistributedPowerSampler inheriting from
+    ``torch.utils.data.DistributedSampler``.
+
+    Samples are sampled with the probability that is proportional to the power
+    of label frequency (freq ^ power). The sampler only applies to single class
+    recognition dataset.
+
+    The default value of power is 1, which is equivalent to bootstrap sampling
+    from the entire dataset.
+    """
+
+    def __init__(self, dataset, num_replicas=None, rank=None, power=1):
+        super().__init__(dataset, num_replicas=num_replicas, rank=rank)
+        self.power = power
+
+    def __iter__(self):
+        # deterministically shuffle based on epoch
+        g = torch.Generator()
+        g.manual_seed(self.epoch)
+
+        # We need to get video_infos_by_class by hand
+        video_infos_by_class = defaultdict(list)
+        key = 'action' if 'action' in self.dataset.video_infos[0] else 'label'
+        for item in self.video_infos:
+            idx = item[key]
+            video_infos_by_class[idx].append(item)
+
+        probs = [
+            len(video_infos_by_class[x[key]]) / len(self.video_infos)
+            for x in self.video_infos
+        ]
+        probs = [prob**self.power for prob in probs]
+
+        indices = torch.multinomial(
+            torch.Tensor(probs),
+            self.total_size,
+            replacement=True,
+            generator=g)
+        indices = indices.data.numpy().tolist()
+        assert len(indices) == self.total_size
+
+        indices = indices[self.rank:self.total_size:self.num_replicas]
+        assert len(indices) == self.num_samples
         return iter(indices)
